aead27bb91678ba9c19c185f0505f16cb5acc547
Richard Thompson <richardthompson1990@hotmail.co.uk>
2019-05-25 12:27:21 +0100

    Updates

---
+++ precache-manifest.3bf2cb06a3f872f698f8e834c240c1aa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c38940ac3cf6b22484e02f8bb8378a1",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "05fb112082f0b7100f50",
+    "url": "/repository-name/static/css/main.d34f547f.chunk.css"
+  },
+  {
+    "revision": "64cb962c9b30fd071e3d",
+    "url": "/repository-name/static/js/2.20e82a78.chunk.js"
+  },
+  {
+    "revision": "05fb112082f0b7100f50",
+    "url": "/repository-name/static/js/main.069f7489.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  }
+]);
